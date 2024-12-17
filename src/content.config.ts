e80d8a4f9f9ceafa2e272b5b66ec18fd5eca493c
Alex Stephen <1325798+rambleraptor@users.noreply.github.com>
2024-12-16 16:22:15 -0800

    Upgrade Starlight (#54)
    * ignore generated files
    
    * upgrading astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,10 @@
 import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader } from "@astrojs/starlight/loaders";
 
 export const collections = {
 	docs: defineCollection({
+		loader: docsLoader(),
 		schema: docsSchema({
 			extend: ({ doc }) => {
 				return z.object({
